4638ac239118493ae03aed1dedd87245e69d34bb
Myriam <mrodrigues3@uclan.ac.uk>
2020-02-07 12:32:45 +0200

    hi

---
+++ Week15Software/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week15Software
+{
+    class Class1
+    {
+        bool isLeap(int year)
+        {
+            return year %4 == 0;
+        }
+    }
+}
